d51a1993a17f9ed80014779274d7a79398fc9658
ocatias <fabian111@gmx.net>
2022-01-11 14:49:26 +0100

    Add helper scripts (testing, duplicates, combine stc+cmc)

---
+++ scripts/check_for_all_files.py
@@ -0,0 +1,60 @@
+"""
+Checks if all necessary files are in the dataset
+Run with "python scripts/check_for_all_files.py"
+"""
+
+import os, glob, json
+
+path_films = r"./films"
+path_auto_annotations = r"./annotations/automatic"
+path_manual_annotations = r"./annotations/manual"
+path_metadata_csv = "metadata.csv"
+path_metadata_json = "metadata.json"
+
+# 97 (NARA) + 3 (EFA) + 8 (LOC) = 108 films in total
+total_nr_films = 108
+duplicates = 9
+nr_films = total_nr_films - duplicates
+
+##########################
+# MAIN SECTION
+##########################
+
+films = glob.glob(os.path.join(path_films, "*.m4v"))
+annotations_auto = glob.glob(os.path.join(path_auto_annotations, "*.json"))
+annotations_manual = glob.glob(os.path.join(path_manual_annotations, "*.json"))
+
+# Check if we have the correct number of files
+assert len(films) == nr_films
+assert len(annotations_auto) == nr_films
+assert len(annotations_manual) == nr_films
+assert os.path.isfile(path_metadata_csv)
+assert os.path.isfile(path_metadata_json)
+print("We have the correct number of files")
+
+# Check that we have an annotation for every film
+annotations_auto_just_id = [os.path.split(path)[-1].split(".")[0] for path in annotations_auto]
+annotations_manual_just_id = [os.path.split(path)[-1].split(".")[0] for path in annotations_manual]
+
+film_ids = [os.path.split(path_film)[-1].split("_")[0] for path_film in films]
+
+for film_id in film_ids:
+    assert film_id in annotations_auto_just_id
+    assert film_id in annotations_manual_just_id
+    annotations_auto_just_id.remove(film_id)
+    annotations_manual_just_id.remove(film_id)
+print("There exist annotations for every film")
+
+# Check if every film is in the metadata files
+with open(path_metadata_csv, "r") as f:
+    lines_from_csv = f.readlines()
+with open(path_metadata_json) as f:
+    list_from_json = json.load(f)
+
+for film_id in film_ids:
+    assert len(list(filter(lambda line: line[0:4] == film_id, lines_from_csv))) == 1
+    assert len(list(filter(lambda dict: dict["id"] == int(film_id), list_from_json))) == 1
+
+print("Every film is mentioned once in each metadata file")
+
+print("All checks complete")
---
+++ scripts/combine_stc_cmc_of_auto_annotations.py
@@ -0,0 +1,51 @@
+"""
+Combine cameraMovement and shotType entries into a single entry in automatic annotations.
+
+E.g. {"inPoint": 1, "outPoint": 257, "cameraMovement": "NA"}, {"inPoint": 1, "outPoint": 257, "shotType": "LS"}
+becomes {"inPoint": 1, "outPoint": 257, "cameraMovement": "NA", "shotType": "LS"}
+
+This makes parsing the JSON files easier.
+"""
+
+import os, glob, json
+
+path_auto_annotations = r"./annotations/automatic"
+
+##########################
+# MAIN SECTION
+##########################
+
+annotation_paths = glob.glob(os.path.join(path_auto_annotations, "*.json"))
+
+for annotation_path in annotation_paths:
+    shots_new = []
+    with open(annotation_path) as f:
+        shots_old = json.load(f)
+
+    for shot in shots_old:
+        # First element
+        if shots_new == []:
+            shots_new.append(shot)
+            continue
+
+        if shots_new[-1]["inPoint"] == shot["inPoint"] and shots_new[-1]["outPoint"] == shot["outPoint"]:
+            if "shotType" in shot:
+                shots_new[-1]["shotType"] = shot["shotType"]
+            else:
+                shots_new[-1]["cameraMovement"] = shot["cameraMovement"]
+        else:
+            shots_new.append(shot)
+
+    # Check if the shots all contain cameraMovement and shotType
+    for shot in shots_new:
+        if "shotType" not in shot:
+            raise ValueError("shotType not found")
+        elif "cameraMovement" not in shot:
+            shot["cameraMovement"] = "NA"
+
+    assert len(shots_new) >= (len(shots_old) / 2.0) and len(shots_new) <= len(shots_old)
+
+    # print(shots_new)
+    # quit()
+    with open(annotation_path, 'w') as outfile:
+        json.dump(shots_new, outfile)
